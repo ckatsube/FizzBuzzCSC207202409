0b0d58d13515bf0509c72fa35615a7740000aefd
ckatsube <=>
2024-09-09 11:46:44 -0400

    add notes for task 1

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,10 +1,42 @@
 /**
  * Solve the FizzBuzz challenge.
+ *
+ * Task 1
+ *
+ * Assignment
+ *  - Types (boolean int void)
+ *
+ * Expressions
+ *  - Operators (% &&)
+ *
+ * Loops
+ *  - Initialization / Condition / Update
+ *
+ * Conditionals
+ *  - if / else if / else
+ *
+ * Functions
+ *  - public static void main
+ *  - Printing
+ *
+ * Syntax
+ *  - brackets
+ *  - indentation
+ *  - comments
  */
 class FizzBuzz {
 
     public static void main(String[] args) {
 
+        /**
+         * How can we convert this for loop to a while loop?
+         *  - Initialization
+         *  - Condition
+         *  - Update
+         *
+         * Food for thought: how does this relate to Python's range function?
+         *  - for i in range(1, 100)
+         */
         for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
